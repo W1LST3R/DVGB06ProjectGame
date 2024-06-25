ddfed06ff1450ba38a3a0a0007b2906699f893a2
W1LST3R <williampet02@gmail.com>
2024-06-25 13:06:13 +0200

    Trap and enemy movement

--- Assets/AngryPig.cs
+++ Assets/AngryPig.cs
@@ -9,6 +9,10 @@ public class AngryPig : MonoBehaviour
     public float x;
     public int distance;
     public bool left = true;
+    public bool playerInsight = false;
+    public bool timeIsRunning = false;
+    public int angryPhase = 10;
+    public float time = 0;
     public Rigidbody2D rigidbody2D;
     // Start is called before the first frame update
     void Start()
@@ -18,8 +22,7 @@ public class AngryPig : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-
+    
     }
 
     private void FixedUpdate()
@@ -43,5 +46,41 @@ public class AngryPig : MonoBehaviour
             }
         }
         rigidbody2D.velocity = new Vector2(speed, rigidbody2D.velocity.y);
+    }
+   
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (!playerInsight)
+            {
+                playerInsight = true;
+                speed *= 3;
+                distance = 30;
+                time = 0;
+            }
+        }  
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerLeftZone();
+        }     
+    }
+
+    private void playerLeftZone()
+    {
+        if(playerInsight)
+        {
+            while(Mathf.FloorToInt(time%60) < angryPhase)
+            {
+                time += Time.deltaTime;
+            }
+            speed /= 3;
+            playerInsight = false;
+            distance = 50;
+           
+        }
     }
 }
--- Assets/SpikeBall.cs
+++ Assets/SpikeBall.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpikeBall : MonoBehaviour
 {
     // Start is called before the first frame update
     public Rigidbody2D rb;
+    public bool start = false;
     void Start()
     {
-        rb.velocity = Vector2.one * 15;
+       startSwing();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (start)
+        {
+            startSwing();
+        }
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -22,6 +27,15 @@ public class SpikeBall : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             GameObject.FindGameObjectWithTag("Player").transform.position = GameObject.FindGameObjectWithTag("Start").transform.position;
+            startSwing();
+        }else if (collision.gameObject.CompareTag("SpikeBall"))
+        {
+            startSwing();
         }
+        
+    }
+    private void startSwing()
+    {
+        rb.velocity = Vector2.one * 15;
     }
 }
